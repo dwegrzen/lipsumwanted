d73134e0abf236ec03f715b19eb55bf6fceacb18
dwegrzen <dwegrzen@gmail.com>
2016-06-20 23:08:00 -0400

    applied fix to index.html add paragraph spacing

--- lipsum.rb
+++ lipsum.rb
@@ -17,16 +17,15 @@ else
   puts "Attempting to generate #{type} text with #{para} paragraph(s)."
 end
 
-
 if type == "bacon"
   title = "Bacon Lorem"
-  content = "Frankfurter ribeye biltong meatball. Pork loin tail sausage rump short loin ham hock. T-bone brisket shankle kielbasa fatback. Chuck rump tail shoulder pig andouille ribeye beef cupim short loin salami brisket. Picanha sausage cow tail hamburger fatback. Meatloaf sirloin doner ground round. Ham hock porchetta t-bone shank turducken drumstick pastrami cupim tail salami pork loin filet mignon sausage pork chop. <br><br>" * para
+  content = "Frankfurter ribeye biltong meatball. Pork loin tail sausage rump short loin ham hock. T-bone brisket shankle kielbasa fatback. Chuck rump tail shoulder pig andouille ribeye beef cupim short loin salami brisket. Picanha sausage cow tail hamburger fatback. Meatloaf sirloin doner ground round. Ham hock porchetta t-bone shank turducken drumstick pastrami cupim tail salami pork loin filet mignon sausage pork chop."
 elsif type == "hipster"
   title = "Hipster Lorem"
-  content = "Next level listicle letterpress, mustache ennui hammock before they sold out freegan photo booth polaroid kinfolk vinyl offal cornhole. Pinterest keytar paleo, vegan lumbersexual mustache bespoke YOLO shoreditch salvia everyday carry biodiesel chambray iPhone. Truffaut fingerstache polaroid, vice semiotics wayfarers deep v lo-fi kitsch swag tumblr banh mi portland. Cronut hella pickled, selvage yr gochujang twee intelligentsia bespoke mumblecore gastropub. Venmo tumblr banh mi paleo tote bag literally. Squid master cleanse cray pop-up pabst, letterpress helvetica marfa retro direct trade yr photo booth cold-pressed. Roof party VHS fingerstache trust fund, mumblecore YOLO umami scenester chambray. <br><br>" * para
+  content = "Next level listicle letterpress, mustache ennui hammock before they sold out freegan photo booth polaroid kinfolk vinyl offal cornhole. Pinterest keytar paleo, vegan lumbersexual mustache bespoke YOLO shoreditch salvia everyday carry biodiesel chambray iPhone. Truffaut fingerstache polaroid, vice semiotics wayfarers deep v lo-fi kitsch swag tumblr banh mi portland. Cronut hella pickled, selvage yr gochujang twee intelligentsia bespoke mumblecore gastropub. Venmo tumblr banh mi paleo tote bag literally. Squid master cleanse cray pop-up pabst, letterpress helvetica marfa retro direct trade yr photo booth cold-pressed. Roof party VHS fingerstache trust fund, mumblecore YOLO umami scenester chambray."
 elsif type == "jackson"
   title = "SLJ Lorem"
-  content = "Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass. <br><br>" * para
+  content = "Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass."
 else
   puts "Please enter a lipsum type: bacon/hipster/jackson and a number of paragraphs (optional, default 1)"
   type = "empty"
